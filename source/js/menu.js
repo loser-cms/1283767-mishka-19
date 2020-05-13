84d551ab55ce3c6d5c1ad5af84c1d4ceac0d907f
Nazar <luceriy87@mail.ru>
2020-05-13 12:40:07 +0300

    Адаптивные декоративные элементы

--- source/js/menu.js
+++ source/js/menu.js
@@ -1,19 +1,18 @@
 var menuToggle = document.querySelector('.header__toggle');
 var header = document.querySelector('.header');
 
-if (header.classList.contains('header__no-js')){
+if (header.classList.contains('header__no-js')) {
   header.classList.remove('header__no-js');
   header.classList.add('header__closed');
 }
 
 
-menuToggle.addEventListener('click', function (){
-  if (header.classList.contains('header__closed')){
+menuToggle.addEventListener('click', function () {
+  if (header.classList.contains('header__closed')) {
     header.classList.remove('header__closed');
     header.classList.add('header__open');
   } else {
     header.classList.add('header__closed');
     header.classList.remove('header__open');
- }
-}
-)
+  }
+})
